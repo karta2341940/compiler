0675a8e98351c2447c0aef5bee4edd2f8611cc08
karta2341940 <a08487a@gmail.com>
2020-11-26 14:40:58 +0800

    sec

--- main.cpp
+++ main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
-int main (std:: string args,int argv)
+
+
+int main (int argv ,char *args[])
 {
-    std::string test = "Hello";
-    std::cout << test << std::endl;
+    std::fstream file;
+
+    std::cout << "Number : " << argv-1 <<std::endl;
+    if(argv > 1)
+    file.open(args[argv-1], std::ios::in|std::ios::out);
+    else 
+    exit(0);
+    std::cout<< args[argv-1] <<std::endl;
+    char str[50];
+   
+    if(file.is_open())
+    std::cout <<"its open"<<std::endl<<std::endl;
+    else 
+    {
+        std::cout <<"open failed"<<std::endl;
+        exit(0);
+    }
+    
+    
+    while (file.getline(str,128,'\n'))
+    {
+        
+        std::cout << str << std::endl;
+    }
+    
+    //std::cout<<"Hello"<<std::endl;
+    file.read(str,1024);
+    std::cin.get();
+    file.close();
+    
+
 }
